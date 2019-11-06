ddcbd21713060b4bffbb5ff219e371ebf553e0ff
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-05 16:15:52 -0800

    added templating index and detail

--- myclub_project/events/urls.py
+++ myclub_project/events/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'events'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView, name='index'),
+    path('<int:event_id>/', views.detail, name='detail'),
 ]
--- myclub_project/events/views.py
+++ myclub_project/events/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
-from django.http import HttpResponse
+from .models import Event
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the club event index.")
+def IndexView(request):
+    event_list = Event.objects.all()
+    context = {'event_list': event_list}
+    return render(request, 'events/index.html', context)
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'events/detail.html', {'event': event})
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
