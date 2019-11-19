0538d63683bd7a944243f6927c881d9b93b099f8
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-19 15:29:50 -0800

    completed tutorial

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,28 @@
 # Register your models here.
 from django.contrib import admin
 
-from .models import Question, Topping, Pizza
+from .models import Question, Topping, Pizza, Choice
 
-admin.site.register(Question)
-admin.site.register(Topping)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 admin.site.register(Pizza)
